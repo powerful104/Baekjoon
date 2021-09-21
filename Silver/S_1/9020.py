72b8782c2e0046938615214421b4b43ba51611b4
Park Sanghyun <p981116@naver.com>
2021-09-21 19:40:21 +0900

    solved by powerful104

---
+++ Silver/S_1/9020.py
@@ -0,0 +1,24 @@
+import sys
+a=0
+b=10000
+li = set(range(2,b+1))
+for i in range(2, b+1):
+    if i in li:
+        li -= set(range(2*i,b+1,i))
+li-=set(range(a))
+li=list(li)
+li.sort()
+
+io=0
+jo=0
+num=int(input())
+for _ in range(num):
+    n=int(sys.stdin.readline())
+    for i in range(len(li)):
+        for j in range(i,len(li)):
+            if li[i]+li[j]>n:
+                break
+            elif li[i]+li[j]==n:
+                io=i
+                jo=j
+    print(min(li[io],li[jo]),max(li[io],li[jo]))
