3f140a4f23682db21a6d7e33d1ab965849650283
Park Sanghyun <p981116@naver.com>
2021-10-02 21:05:34 +0900

    solved by powerful104

---
+++ Silver/S_2/11047.py
@@ -0,0 +1,11 @@
+n, k= map(int, input().split())
+li=[]
+mod=k
+ans=0
+for _ in range(n):
+    li.append(int(input()))
+li.sort(reverse=True)
+for i in li:
+    ans+=mod//i
+    mod=mod%i
+print(ans)
