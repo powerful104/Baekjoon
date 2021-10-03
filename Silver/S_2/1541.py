3dc26df7bf12c14e79f5537d25d9c51f13a29883
Park Sanghyun <p981116@naver.com>
2021-10-03 15:32:06 +0900

    solved by powerful104

---
+++ Silver/S_2/1541.py
@@ -0,0 +1,12 @@
+import re
+s = input()
+num = list(map(int, re.split('[-+]',s)))
+exp = ' '.join(re.split('[0123456789]',s)).split()
+cur = 0
+while cur != len(exp):
+    if exp[cur] == '+':
+        num[cur:cur+2] = [num[cur]+num[cur+1]]
+        exp[cur:cur+1] = []
+    else:
+        cur += 1
+print(num[0]-sum(num[1:]))
