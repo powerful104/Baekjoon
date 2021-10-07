fa6c497b94c453cf4224822376abeddeaf321f81
Park Sanghyun <p981116@naver.com>
2021-10-07 21:14:04 +0900

    solving by powerful104

---
+++ Silver/S_2/2504.py
@@ -0,0 +1,16 @@
+s = input()
+
+LArr = []
+answer = False
+for j in s:
+    if len(LArr) != 0 and ((LArr[-1] == '(' and j == ')')):
+        LArr.pop()
+    else:
+        LArr.append(j)
+
+if len(LArr) == 0:
+    answer = True
+
+"""
+
+"""
