644d6a65f71257cdf8178118897e2d887dbdb000
Park Sanghyun <p981116@naver.com>
2021-10-10 21:06:02 +0900

    solved by powerful104

---
+++ Silver/S_3/10799.py
@@ -0,0 +1,13 @@
+s= input()
+s = s.replace('()','a')
+cur=0
+ans=0
+for i in list(s):
+    if i=='a':
+        ans+=cur
+    elif i=='(':
+        cur+=1
+        ans+=1
+    else:
+        cur-=1
+print(ans)
