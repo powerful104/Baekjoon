936b3f55dbd3f9e57493a0d28b9f20a5c144f843
Park Sanghyun <p981116@naver.com>
2021-10-10 21:05:54 +0900

    solved by powerful104

---
+++ Silver/S_3/9613.py
@@ -0,0 +1,9 @@
+import math as m
+num = int(input())
+for _ in range(num):
+    li = list(map(int, input().split()))
+    ans=0
+    for i in range(1,len(li)-1):
+        for j in range(i+1,len(li)):
+            ans+=m.gcd(li[i],li[j])
+    print(ans)
