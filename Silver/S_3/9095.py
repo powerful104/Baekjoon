412713182066079a60a1f89fd590fc6a2becaa3a
Park Sanghyun <p981116@naver.com>
2021-10-16 23:10:12 +0900

    solved by powerful104

---
+++ Silver/S_3/9095.py
@@ -0,0 +1,13 @@
+import sys
+def P(num):
+    if num==1:
+        return 1
+    elif num==2:
+        return 2
+    elif num==3:
+        return 4
+    else:
+        return P(num-3)+P(num-2)+P(num-1)
+num = int(input())
+for _ in range(num):
+    print(P(int(sys.stdin.readline())))
