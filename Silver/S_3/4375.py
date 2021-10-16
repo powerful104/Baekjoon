f52696ea4e8934ddd9122c95b0f68c0666b935d8
Park Sanghyun <p981116@naver.com>
2021-10-16 23:10:05 +0900

    solved by powerful104

---
+++ Silver/S_3/4375.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        num = int(input())
+        s=0
+        for _ in range(len(str(num))):
+            s=s*10+1
+        while True:
+            if s%num==0:
+                break
+            s=s*10+1
+        print(len(str(s)))
+    except EOFError:
+        break
