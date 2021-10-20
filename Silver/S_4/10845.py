c2ed5724959e4416f6ae4f65da5f3b73f7cc1c47
Park Sanghyun <p981116@naver.com>
2021-10-20 17:41:02 +0900

    solved by powerful104

---
+++ Silver/S_4/10845.py
@@ -0,0 +1,27 @@
+import sys
+num = int(input())
+stack=[]
+for _ in range(num):
+    li = list(map(str, sys.stdin.readline().split()))
+    n=0
+    if li[0]=="push":
+        stack.append(int(li[1]))
+    elif li[0] == "pop":
+        if len(stack)!=0:
+            print(stack.pop(0))
+        else:
+            print("-1")
+    elif li[0] == "size":
+        print(len(stack))
+    elif li[0] == "empty":
+        print(int(len(stack)==0))
+    elif li[0] == "front":
+        if len(stack)!=0:
+            print(stack[0])
+        else:
+            print("-1")
+    else:
+        if len(stack)!=0:
+            print(stack[-1])
+        else:
+            print("-1")
