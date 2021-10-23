3d0b1f9c419e52c706efeab5005407ae1e1567de
Park Sanghyun <p981116@naver.com>
2021-10-23 23:13:18 +0900

    solved by powerful104

---
+++ Silver/S_5/11576.py
@@ -0,0 +1,15 @@
+a,b= map(int, input().split())
+m = int(input())
+li = list(map(int, input().split()))
+li.reverse()
+num=0
+ra=1
+for i in li:
+    num+=i*ra
+    ra*=a
+lit=[]
+while num!=0:
+    lit.append(num%b)
+    num=num//b
+lit.reverse()
+print(" ".join(map(str,lit)))
