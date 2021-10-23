15b30f0175c99fba1b43009593384bab7d134576
Park Sanghyun <p981116@naver.com>
2021-10-23 23:13:25 +0900

    solved by powerful104

---
+++ Silver/S_5/11723.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+se=set()
+for _ in range(num):
+    li = list(map(str, input().split()))
+    c = li[0] #command
+    if c=="add":
+        se.add(int(li[1]))
+    elif c=="remove":
+        if int(li[1]) in se:
+            se.remove(int(li[1]))
+    elif c=="check":
+        print(int(int(li[1]) in se))
+    elif c=="toggle":
+        if int(li[1]) in se:
+            se.remove(int(li[1]))
+        else:
+            se.add(int(li[1]))
+    elif c=="all":
+        se=set(range(1,21))
+    else:
+        se.clear()
