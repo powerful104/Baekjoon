9be44f5cd8c350d80e034016ab41f9dfc35ed953
Park Sanghyun <p981116@naver.com>
2021-10-28 21:45:23 +0900

    solved by powerful104

---
+++ Silver/S_3/1966.py
@@ -0,0 +1,16 @@
+num = int(input())
+for _ in range(num):
+    n, m = map(int, input().split())
+    li = list(map(int, input().split()))
+    doc = [0]*n
+    doc[m]=1
+    ans=0
+    while sum(doc)!=0:
+        if li[0]==max(li):
+            li.pop(0)
+            doc.pop(0)
+            ans+=1
+        else:
+            li.append(li.pop(0))
+            doc.append(doc.pop(0))
+    print(ans)
