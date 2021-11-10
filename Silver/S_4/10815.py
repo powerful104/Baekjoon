549aa5baf2747a1f6f3dd2ed2d89a8d93aab3d3e
Park Sanghyun <p981116@naver.com>
2021-11-10 23:26:09 +0900

    solved by powerful104

---
+++ Silver/S_4/10815.py
@@ -0,0 +1,11 @@
+li = [0] * 20000001
+num = int(input())
+lia = list(map(int, input().split()))
+for i in lia:
+    li[i+10000000]=1
+n = int(input())
+lib = list(map(int, input().split()))
+for i in range(n):
+    print(li[lib[i]+10000000],end="")
+    if i!=n:
+        print(" ",end="")
