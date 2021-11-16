23e570f716be96ebc1dd7386b10718f2f003829d
Park Sanghyun <p981116@naver.com>
2021-11-16 22:38:49 +0900

    solved by powerful104

---
+++ Silver/S_3/1463.py
@@ -0,0 +1,14 @@
+def P(num):
+    li=[1]*(num+1)
+    li[1]=0
+    for i in range(4,num+1):
+        lit=[]
+        if i%3==0:
+            lit.append(li[i//3])
+        if i%2==0:
+            lit.append(li[i//2])
+        lit.append(li[i-1])
+        li[i]=min(lit)+1
+    return li[num]
+n = int(input())
+print(P(n))
