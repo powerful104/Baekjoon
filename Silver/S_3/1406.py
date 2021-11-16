69e234b0b1b5ed2c803c3e753bc815931388bdff
Park Sanghyun <p981116@naver.com>
2021-11-16 22:38:46 +0900

    solved by powerful104

---
+++ Silver/S_3/1406.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+left=[]
+right=[]
+left+=list(input().strip())
+num = int(input())
+for _ in range(num):
+    li = list(map(str, input().split()))
+    if li[0] == "L":
+        if len(left)>0:
+            right.append(left[-1])
+            del left[-1]
+    elif li[0] == "D":
+        if len(right)>0:
+            left.append(right[-1])
+            del right[-1]
+    elif li[0] == "B":
+        if len(left)>0:
+            del left[-1]
+    else:
+        left.append(li[1])
+right.reverse()
+print("".join(left)+"".join(right))
