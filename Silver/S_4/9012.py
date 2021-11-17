614fd9ed265e41500be78d31cb4778ea300e2c00
Park Sanghyun <p981116@naver.com>
2021-11-17 22:36:45 +0900

    solved by powerful104

---
+++ Silver/S_4/9012.py
@@ -0,0 +1,17 @@
+num = int(input())
+for _ in range(num):
+    s=str(input())
+    ans=0
+    check=0
+    for i in range(len(s)):
+        if s[i]=="(":
+            ans+=1
+        elif s[i]==")":
+            ans-=1
+        if ans<0:
+            check=1
+            break
+    if ans>0 or check==1:
+        print("NO")
+    else:
+        print("YES")
