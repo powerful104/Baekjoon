4c747013624f95350088ce32f5dc893022e696bf
Park Sanghyun <p981116@naver.com>
2021-12-18 19:48:42 +0900

    solved by powerful104

---
+++ Silver/S_2/4948.py
@@ -0,0 +1,10 @@
+while True:
+    num = int(input())
+    if num==0:
+        break
+    li = set(range(2,2*num+1))
+    for i in range(2,2*num+1):
+        if i in li:
+            li -= set(range(2*i,2*num+1,i))
+    li-=set(range(num+1))
+    print(len(li))
