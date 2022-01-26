3d9e74f1d508c4b790377f918c1ecc812338359f
Park Sanghyun <76836510+powerful104@users.noreply.github.com>
2022-01-26 22:20:26 +0900

    solved by powerful104

--- Silver/S_5/10867.py
+++ Silver/S_5/10867.py
@@ -1,8 +1,10 @@
 lia = [0] * 2001
 num = int(input())
 li = list(map(int, input().split()))
+
 for i in li:
     lia[i+1000]=1
+    
 for i in range(len(lia)):
     if lia[i]==1:
         print(i-1000,end="")
