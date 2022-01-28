db5024f8bcc3a39729a8a260c575ae83c7c1f3f0
Park Sanghyun <76836510+powerful104@users.noreply.github.com>
2022-01-28 22:35:44 +0900

    solved by powerful104

--- Silver/S_5/11650.py
+++ Silver/S_5/11650.py
@@ -1,9 +1,11 @@
 import sys
 num = int(input())
 li=[]
+
 for i in range(num):
     li.append(list(map(int,sys.stdin.readline().split())))
 li = sorted(li, key=lambda x : (x[0], x[1]))
+
 for i in li:
     for k in range(2):
         print(i[k],end=" ")
