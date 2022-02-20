0dfb387598cf8ec9f91b246533f5eee63b499a51
Park Sanghyun <p981116@naver.com>
2022-02-20 16:03:40 +0900

    solved by powerful104

---
+++ Silver/S_3/1072.py
@@ -0,0 +1,17 @@
+import math
+a,b= map(int, input().split())
+ans=0
+z=math.floor(100*b/a)
+low, high = 0, 1000000000
+if z>=99:
+    ans=-1
+else:
+    while low <= high:
+        mid = (low + high) // 2
+        ta, tb = a + mid, b + mid
+        if math.floor(100 * tb / ta) > z:
+            high = mid - 1
+        else:
+            low = mid + 1
+    ans=high+1
+print(ans)
