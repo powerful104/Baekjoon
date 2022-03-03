60e605b1471bd04e4cb47c66d8b7db5b2df3a743
powerful104 <p981116@naver.com>
2022-03-03 23:56:14 +0900

    solved by powerful104

--- Silver/S_1/5567.py
+++ Silver/S_1/5567.py
@@ -9,12 +9,14 @@ for _ in range(numa):
     li.append([min(a,b),max(a,b)])
 li.sort()
 check=0
+
 for i in li:
     if i[0]!=1:
         break
     sa.add(i[1])
     sf.add(i[1])
     check+=1
+    
 for i in range(check,len(li)):
     if li[i][0] in sa:
         sf.add(li[i][1])
