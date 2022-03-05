d3e69f14d1f52c559cc78e510fde4fa5a6cfe7dc
powerful104 <p981116@naver.com>
2022-03-05 16:15:43 +0900

    solved by powerful104

--- Silver/S_4/10816.py
+++ Silver/S_4/10816.py
@@ -1,11 +1,13 @@
 num = int(input())
 li = list(map(int, input().split()))
 d={}
+
 for i in li:
     d.setdefault(i,0)
     d[i]+=1
 n = int(input())
 lit = list(map(int, input().split()))
+
 for i in range(n):
     d.setdefault(lit[i],0)
     print(d[lit[i]],end="")
