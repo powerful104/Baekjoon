74a3d0834b0bde51cdf462ca2035c2bd8f65c5be
Park Sanghyun <p981116@naver.com>
2022-03-07 23:35:06 +0900

    solved by powerful104

---
+++ Silver/S_5/7785.py
@@ -0,0 +1,13 @@
+import sys
+num=int(input())
+li={}
+for _ in range(num):
+    a, b = map(str, sys.stdin.readline().split())
+    if b=="enter":
+        li[a]=True
+    else:
+        li[a]=False
+d=sorted(li.keys(),reverse=True)
+for i in d:
+    if li[i]==True:
+        print(i)
