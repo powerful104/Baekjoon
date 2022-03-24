ac179fd5ebd54d1e956fd82d0fde5d49d820a509
Park Sanghyun <p981116@naver.com>
2022-03-24 23:38:01 +0900

    solved by powerful104

--- Silver/S_2/5567.py
+++ Silver/S_2/5567.py
@@ -3,17 +3,21 @@ numa = int(input())
 sa=set()
 sf=set()
 li=[]
+
 for _ in range(numa):
     a, b = map(int, input().split())
     li.append([min(a,b),max(a,b)])
+
 li.sort()
 check=0
+
 for i in li:
     if i[0]!=1:
         break
     sa.add(i[1])
     sf.add(i[1])
     check+=1
+
 for i in range(check,len(li)):
     if li[i][0] in sa:
         sf.add(li[i][1])
