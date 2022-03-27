b2a10892c8b27cd436939771349cca3364fce859
Park Sanghyun <76836510+powerful104@users.noreply.github.com>
2022-03-27 22:47:29 +0900

    solved by powerful104

--- Silver/S_2/6603.py
+++ Silver/S_2/6603.py
@@ -2,11 +2,13 @@ import itertools as ite
 
 while True:
     li = list(map(int, input().split()))
+    
     if li[0]==0:
         break
     num=li[0]
     del li[0]
     lit = ite.combinations(li,6)
+    
     for i in lit:
         print(" ".join(map(str,i)))
     print()
