056da1dbc0ba1c086956d397fac29008f896df90
Park Sanghyun <p981116@naver.com>
2022-03-30 23:38:04 +0900

    solved by powerful104

--- Silver/S_2/14494.py
+++ Silver/S_2/14494.py
@@ -1,10 +1,14 @@
 a,b= map(int, input().split())
 li = [[0 for col in range(a) ] for row in range(b)]
+
 for i in range(a):
     li[0][i]=1
+    
 for j in range(b):
     li[j][0]=1
+    
 for i in range(1,b):
     for j in range(1,a):
         li[i][j]=(li[i-1][j]+li[i][j-1]+li[i-1][j-1])%1000000007
+        
 print(li[b-1][a-1])
