fff54285755dd9d7a92cdfc8483ec5254de5a762
Park Sanghyun <p981116@naver.com>
2022-04-02 23:25:39 +0900

    solved by powerful104

--- Silver/S_2/17829.py
+++ Silver/S_2/17829.py
@@ -1,14 +1,18 @@
 li=[]
 n=int(input())
+
 for _ in range(n):
     li+=list(map(int, input().split()))
+    
 while n!=1:
     litmp=[]
+    
     for j in range(0,n,2):
         for i in range(0,n,2):
             lic=[li[i+j*n],li[i+j*n+1],li[i+j*n+n],li[i+j*n+n+1]]
             lic.sort()
             litmp.append(lic[2])
+            
     li=list(litmp)
     n=n//2
 print(li[0])
