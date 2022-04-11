884e23d33036e7840663542ef55d318aae56cc46
Park Sanghyun <p981116@naver.com>
2022-04-11 23:58:55 +0900

    solved by powerful104

--- Silver/S_4/8922.py
+++ Silver/S_4/8922.py
@@ -8,12 +8,15 @@ for _ in range(tc):
     while True:
         tl=[]
         t=0
+        
         for i in range(len(li[-1])):
             if i==0:
                 t=abs(li[-1][i-1]-li[-1][i])
             else:
                 tl.append(abs(li[-1][i-1]-li[-1][i]))
+                
         tl.append(t)
+        
         if sum(tl)==0:
             print("ZERO")
             break
