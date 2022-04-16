28a8a93a122dfcf489d202da1a69ee65b4f0b026
Park Sanghyun <p981116@naver.com>
2022-04-16 22:30:24 +0900

    solved by powerful104

---
+++ Silver/S_4/4949.py
@@ -0,0 +1,28 @@
+while True:
+    s = input()
+    if s == ".":
+        break
+    st=""
+    for i in list(s):
+        if i == "(":
+            st+=i
+        elif i == ")":
+            st+=i
+        elif i == "[":
+            st+=i
+        elif i == "]":
+            st+=i
+    s=str(st)
+    while "()" in s or "[]" in s:
+        if "()" in s:
+            a = s[:s.find("()")]
+            b = s[s.find("()") + len("()"):]
+            s = a + b
+        if "[]" in s:
+            a = s[:s.find("[]")]
+            b = s[s.find("[]") + len("[]"):]
+            s = a + b
+    if len(s)!=0:
+        print("no")
+    else:
+        print("yes")
