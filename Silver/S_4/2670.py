41122687df63065617aaafc74b7ce5ffd15eefc3
Park Sanghyun <p981116@naver.com>
2022-04-17 16:06:30 +0900

    solved by powerful104

--- Silver/S_4/2670.py
+++ Silver/S_4/2670.py
@@ -1,9 +1,12 @@
 num = int(input())
 li=[]
+
 for _ in range(num):
     li.append(float(input()))
+    
 result = li[0]
 cur = 1
+
 for i in range(0, num):
     cur *= li[i]
     result = max(result, cur)
