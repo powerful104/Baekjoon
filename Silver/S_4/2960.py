28a2f9416e14d600449b2770c7ac39874a8a6cd0
Park Sanghyun <p981116@naver.com>
2022-04-18 23:52:56 +0900

    solved by powerful104

--- Silver/S_4/2960.py
+++ Silver/S_4/2960.py
@@ -2,6 +2,7 @@ a,b= map(int, input().split())
 li=list(range(2,a+1))
 mi=0
 ans=0
+
 while True:
     c=min(li)
     mi+=1
@@ -10,6 +11,7 @@ while True:
         ans=c
         break
     del li[0]
+    
     for i in li:
         if i%c==0:
             del li[li.index(i)]
@@ -18,6 +20,7 @@ while True:
                 ans = i
                 check=1
                 break
+            
     if check==1:
         break
 print(ans)
