4cf59485657c3627d5d161ae1b0615c12de039c8
Park Sanghyun <p981116@naver.com>
2022-05-29 22:37:44 +0900

    solved by powerful104

--- Gold/G_3/10942.py
+++ Gold/G_3/10942.py
@@ -1,24 +1,23 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 num_List = list(map(int, input().split()))
 M = int(input())
 
 dp = [[0] * (N + 1) for _ in range(N + 1)]
 
-def is_palin(num_List):
-    check = 1
-    for i in range(len(num_List)):
-        if num_List[i] != num_List[len(num_List)-1-i]:
-            check = 0
-    return check
-
-for j in range(1,N+1):
-    for i in range(j-1,N):
-        if dp[j-1][i+1] == 0:
-            dp[j][i] = is_palin(num_List[j-1:i+1])
+for E in range(1,N+1):
+    for S in range(1,E+1):
+        if E-S <= 1:
+            if num_List[E-1] == num_List[S-1]:
+                dp[S][E] = 1
         else:
-            dp[j][i] = 1
-
-
+            if dp[S+1][E-1] == 1 and num_List[E-1] == num_List[S-1]:
+                dp[S][E] = 1
+            else:
+                dp[S][E] = 0
+                
 for _ in range(M):
     S, E = map(int, input().split())
-    print(dp[S][E-1])
+    print(dp[S][E])
