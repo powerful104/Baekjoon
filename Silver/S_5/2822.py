34cedc82f1e729e59039c6ca218f125c443f8434
Park Sanghyun <p981116@naver.com>
2022-06-20 16:06:32 +0900

    update

---
+++ Silver/S_5/2822.py
@@ -0,0 +1,13 @@
+points = []
+for i in range(1,9):
+    points.append([int(input()), i])
+points.sort(reverse = True)
+last = 0
+for point in points[:5]:
+    last += point[0]
+print(last)
+numbers = []
+for point in points[:5]:
+    numbers.append(point[1])
+numbers.sort()
+print(' '.join(map(str, numbers)))
