e9dbf760c086ec3e35f9566a67629ced0db399b9
Park Sanghyun <p981116@naver.com>
2022-06-20 16:07:31 +0900

    solved 2822 by powerful104

--- Silver/S_5/2822.py
+++ Silver/S_5/2822.py
@@ -1,12 +1,15 @@
 points = []
+
 for i in range(1,9):
     points.append([int(input()), i])
 points.sort(reverse = True)
 last = 0
+
 for point in points[:5]:
     last += point[0]
 print(last)
 numbers = []
+
 for point in points[:5]:
     numbers.append(point[1])
 numbers.sort()
