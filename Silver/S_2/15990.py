31678a9ccdcc54e6c4d60ab93a551391d61dacf1
Park Sanghyun <p981116@naver.com>
2022-06-20 16:06:13 +0900

    solved by powerful104

--- Silver/S_2/15990.py
+++ Silver/S_2/15990.py
@@ -2,12 +2,14 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
+max_li = 3
+dp = [[1,0,0],[0,1,0],[1,1,1]] + [[0,0,0] for _ in range(100000-3)]
+
+for i in range(3, 100000):
+    for j in range(1, 4):
+        dp[i][j-1] = sum(dp[i-j]) - dp[i-j][j-1]
+        dp[i][j-1] %= 1000000009
+
 for _ in range(T):
     n = int(input())
-    dp = [[[1],[1],[1]]] + [[0],[0],[0]] * n
-    for i in range(1, 4):
-        for j in range(i,n+1):
-            dp[j] += dp[j-i]
-            dp[j] %= 1000000009
-    print(dp[n])
-    
+    print(sum(dp[n-1])%1000000009)
