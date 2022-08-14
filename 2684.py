09ec7072c410a8e8dcc2efbdaa9ae39df8df051e
Park Sanghyun <p981116@naver.com>
2022-08-14 11:54:30 +0900

    solved 2684 by powerful104

---
+++ 2684.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+dic = {'TTT' : 0, 'TTH' : 1, 'THT' : 2, 'THH' : 3, 
+       'HTT' : 4, 'HTH' : 5, 'HHT' : 6, 'HHH' : 7, }
+
+for _ in range(N):
+    s = input()
+    ans = [0] * 8
+    for i in range(38):
+        ans[dic[s[i:i+3]]] += 1
+            
+    print(" ".join(list(map(str, ans))))
