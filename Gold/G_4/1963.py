461eddafacd48c7df99d77dbd4bf9040170483af
powerful104 <p981116@naver.com>
2022-10-07 21:34:39 +0900

    solved 1963 by powerful104

--- Gold/G_4/1963.py
+++ Gold/G_4/1963.py
@@ -1,14 +1,14 @@
+from collections import deque
+
 def prime_list(n):
-    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
     sieve = [True] * n
 
     m = int(n ** 0.5)
     for i in range(2, m + 1):
-        if sieve[i] == True:           # i가 소수인 경우 
-            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+        if sieve[i] == True:
+            for j in range(i+i, n, i):
                 sieve[j] = False
 
-    # 소수 목록 산출
     return [i for i in range(2, n) if sieve[i] == True]
 
 def change_poss(num1, num2):
@@ -17,35 +17,45 @@ def change_poss(num1, num2):
         if str(num1)[i] == str(num2)[i]:
             check += 1
     
-    if check == 1:
+    if check == 3:
         return True
     else:
         return False
 
-from collections import deque
-
-# BFS
-def bfs(graph, start, visited):
-	# 큐 구현을 위해 deque 라이브러리 사용
-	queue = deque([start])
-	# 현재 노드를 방문 처리
-	visited[start] = True
-
-	# 큐가 빌때까지 반복
-	while queue:
-		v = queue.popleft()
-		print(v, end=' ')
-		# 해당 원소와 인접한, 아직 방문하지 않은 원소들을 큐에 삽입
-		for i in graph[v]:
-			if not visited[i]:
-				queue.append(i)
-				visited[i] = True
-
 li = prime_list(10001)[168:]
 
+nums_dic = {}
 graph = [[] for _ in range(len(li))]
 
+def bfs(start, goal, visited):
+    queue = deque([nums_dic[start]])
+    visited[nums_dic[start]] = 1
+    
+    if start == goal:
+        print(0)
+        return
+    
+    ans = 1
+    while queue:
+        tmpq = deque()
+        for v in queue:
+            for i in graph[v]:
+                if visited[i] != 1:
+                    if li[i] == goal:
+                        print(ans)
+                        return
+                    tmpq.append(i)
+                    visited[i] = 1
+        queue = tmpq
+        ans += 1
+
 for i in range(len(li)):
+    nums_dic[li[i]] = i
     for j in range(len(li)):
         if change_poss(li[i], li[j]):
             graph[i].append(j)
+
+for _ in range(int(input())):
+    visi = [0 for _ in range(1061)]
+    num1, num2 = map(int, input().split())
+    bfs(num1, num2, visi)
